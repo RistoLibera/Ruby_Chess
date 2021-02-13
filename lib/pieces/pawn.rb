c4a72fcaa230285cd78b9a307ca5b9d7fe7c3b96
qofgxnm7 <qofgxnm7@gmail.com>
2021-02-13 06:29:09 +0000

    Finish pawn movable function

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,17 +3,16 @@ require_relative "../display.rb"
 class Pawn
     attr_reader :color
     attr_reader :location
+    attr_reader :movable_space
     include Display
 
     def initialize(color, location)
         @location = location
         @color = color
         #pawn moves forward by one or two in the first step, only one in the nexts.
-        #space needed to be selected.
-        @white_move = [[-1, 0], [-2, 0]]
-        @black_move = [[+1, 0], [+2 ,0]]
         @movable_space = []
         @replace = []
+        @has_promotion = true
     end
     
     def push_unicode
@@ -30,23 +29,25 @@ class Pawn
         return feedback   
     end
 
+    #when in the original co-ord has the choice to move one or two
     def update_space(board)
-        @move.each do |move|
-            row = move[0] + @location[0]
-            column = move[1] + @location[1]
-            while row.between?(0, 7) && column.between?(0, 7) do
-                position = board[row][column]
-                if position == ""
-                    @movable_space << [row,column]
-                    row += move[0]
-                    column += move[1]
-                elsif position.color != @color
-                    @movable_space << [row,column]
-                    break
-                else
-                    break
-                end
-            end
-        end
+        row = @location[0]
+        column = @location[1]
+        case @color
+        when "black"
+            @movable_space << [2, column] if row == 1 && board[2][column] == ""
+            @movable_space << [3, column] if row == 1 && board[3][column] == ""
+            @movable_space << [row + 1, column] if (row + 1).between?(0, 7) && board[row + 1][column] == ""
+            @movable_space << [row + 1, column - 1] if board[row + 1][column - 1] != ""
+            @movable_space << [row + 1, column + 1] if board[row + 1][column + 1] != ""
+        when "white"
+            @movable_space << [5, column] if row == 6 && board[5][column] == ""
+            @movable_space << [4, column] if row == 6 && board[4][column] == ""
+            @movable_space << [row - 1, column] if (row - 1).between?(0, 7) && board[row - 1][column] == ""
+            @movable_space << [row - 1, column - 1] if board[row - 1][column - 1] != ""
+            @movable_space << [row - 1, column + 1] if board[row - 1][column + 1] != ""
+        else
+            puts "Error!"
+        end    
     end
 end
